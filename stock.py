5b8cb1d38664ff7145b94ec92a5ea5db6b64c872
minocrafft <minocrafft@gmail.com>
2023-11-07 23:01:31 +0900

    Complete ~exercise 4.4

--- stock.py
+++ stock.py
@@ -1,4 +1,5 @@
 from decimal import Decimal
+from validate import PositiveInteger, PositiveFloat, String
 
 
 def print_portfolio(portfolio):
@@ -10,8 +11,9 @@ def print_portfolio(portfolio):
 
 
 class Stock:
-    __slots__ = ("name", "_shares", "_price")
-    _types = (str, int, float)
+    name = String()
+    shares = PositiveInteger()
+    price = PositiveFloat()
 
     def __init__(self, name, shares, price):
         self.name = name
@@ -21,34 +23,6 @@ class Stock:
     @classmethod
     def from_row(cls, row):
         return cls(*[func(val) for func, val in zip(cls._types, row)])
-
-    @property
-    def shares(self):
-        return self._shares
-
-    @shares.setter
-    def shares(self, value):
-        if not isinstance(value, self._types[1]):
-            raise TypeError(f"Expected {self._types[1].__name__}")
-
-        if value < 0:
-            raise ValueError("shares must be >= 0")
-
-        self._shares = value
-
-    @property
-    def price(self):
-        return self._price
-
-    @price.setter
-    def price(self, value):
-        if not isinstance(value, self._types[2]):
-            raise TypeError(f"Expected {self._types[2].__name__}")
-
-        if value < 0:
-            raise ValueError("price must be >= 0")
-
-        self._price = value
 
     @property
     def cost(self):
@@ -61,6 +35,11 @@ class Stock:
         # Note: The !r format code produces the repr() string
         return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
 
+    def __setattr__(self, name, value):
+        if name not in {"name", "shares", "price"}:
+            raise AttributeError("No attribute %s" % name)
+        super().__setattr__(name, value)
+
     def __eq__(self, other):
         return isinstance(other, Stock) and (
             (self.name, self.shares, self.price)
@@ -70,3 +49,24 @@ class Stock:
 
 class DStock(Stock):
     _types = (str, int, Decimal)
+
+
+class SimpleStock:
+    def __init__(self, name, shares, price):
+        self.name = name
+        self.shares = shares
+        self.price = price
+
+    def cost(self):
+        return self.shares * self.price
+
+
+class Readonly:
+    def __init__(self, obj):
+        self.__dict__["_obj"] = obj
+
+    def __setattr__(self, name, value):
+        raise AttributeError("Can't set attribute")
+
+    def __getattr__(self, name):
+        return getattr(self._obj, name)
