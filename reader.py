061c80e0f0c75be188cc91df8d77e00ab4130c50
minocrafft <minocrafft@gmail.com>
2023-11-12 16:44:52 +0900

    Complete ~exercise 5.6

--- reader.py
+++ reader.py
@@ -1,7 +1,11 @@
 import csv
+import logging
 from abc import ABC, abstractmethod
 from collections import defaultdict
 from typing import Callable
+
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
 
 
 class DataCollection:
@@ -48,38 +52,50 @@ class InstanceCSVParser(CSVParser):
         return self.cls.from_row(row)
 
 
-def read_csv_as_dicts(filename, types: list[Callable] = [str, int, float]):
-    """Read the bus ride data as a list of dicts"""
-    parser = DictCSVParser(types)
-    return parser.parse(filename)
+def convert_csv(lines, converter, *, headers=None):
+    rows = csv.reader(lines)
+    if headers is None:
+        headers = next(rows)
+
+    records = []
+    for i, row in enumerate(rows, 1):
+        try:
+            records.append(converter(headers, row))
+        except ValueError as e:
+            logger.warning(f"Row {i}: Bad row: {row}")
+            logger.debug(f"Row {i}: Reason: {e}")
+
+    return records
 
 
-def read_csv_as_columns(filename, types: list[Callable]):
+def csv_as_dicts(lines, types, *, headers=None):
+    return convert_csv(
+        lines,
+        lambda headers, row: {
+            name: func(val) for name, func, val in zip(headers, types, row)
+        },
+    )
+
+
+def csv_as_instances(lines, cls, *, headers=None):
+    return convert_csv(lines, lambda headers, row: cls.from_row(row))
+
+
+def read_csv_as_dicts(filename, types, *, headers=None):
     """
-    Read the bus ride data into 4 lists, representing columns
+    Read CSV data into a list of dictionaries with optional type conversion
     """
-    datas = defaultdict(list)
-    with open(filename) as f:
-        rows = csv.reader(f)
-        headers = next(rows)  # Skip headers
-
-        for row in rows:
-            for name, func, val in zip(headers, types, row):
-                datas[name].append(func(val))
-
-    return DataCollection(datas)
+    with open(filename) as file:
+        return csv_as_dicts(file, types, headers=headers)
 
 
-def read_csv_as_instances(filename, cls):
-    """Read a CSV file into a list of instances"""
-    parser = InstanceCSVParser(cls)
-    return parser.parse(filename)
+def read_csv_as_instances(filename, cls, *, headers=None):
+    """
+    Read CSV data into a list of instances
+    """
+    with open(filename) as file:
+        return csv_as_instances(file, cls, headers=headers)
 
 
 if __name__ == "__main__":
-    import tracemalloc
-    from sys import intern
-
-    tracemalloc.start()
-    data = read_csv_as_columns("Data/ctabus.csv", [intern, intern, intern, int])
-    print(tracemalloc.get_traced_memory())
+    port = read_csv_as_dicts("Data/missing.csv", types=[str, int, float])
--- stock.py
+++ stock.py
@@ -11,40 +11,59 @@ def print_portfolio(portfolio):
 
 
 class Stock:
-    name = String()
-    shares = PositiveInteger()
-    price = PositiveFloat()
+    __slots__ = ("name", "_shares", "_price")
+    _types = (str, int, float)
 
     def __init__(self, name, shares, price):
         self.name = name
         self.shares = shares
         self.price = price
 
-    @classmethod
-    def from_row(cls, row):
-        return cls(*[func(val) for func, val in zip(cls._types, row)])
-
-    @property
-    def cost(self):
-        return self.shares * self.price
-
-    def sell(self, n):
-        self.shares -= n
-
     def __repr__(self):
         # Note: The !r format code produces the repr() string
         return f"{type(self).__name__}({self.name!r}, {self.shares!r}, {self.price!r})"
-
-    def __setattr__(self, name, value):
-        if name not in {"name", "shares", "price"}:
-            raise AttributeError("No attribute %s" % name)
-        super().__setattr__(name, value)
 
     def __eq__(self, other):
         return isinstance(other, Stock) and (
             (self.name, self.shares, self.price)
             == (other.name, other.shares, other.price)
         )
+
+    @classmethod
+    def from_row(cls, row):
+        values = [func(val) for func, val in zip(cls._types, row)]
+        return cls(*values)
+
+    @property
+    def shares(self):
+        return self._shares
+
+    @shares.setter
+    def shares(self, value):
+        if not isinstance(value, self._types[1]):
+            raise TypeError(f"Expected {self._types[1].__name__}")
+        if value < 0:
+            raise ValueError("shares must be >= 0")
+        self._shares = value
+
+    @property
+    def price(self):
+        return self._price
+
+    @price.setter
+    def price(self, value):
+        if not isinstance(value, self._types[2]):
+            raise TypeError(f"Expected {self._types[2].__name__}")
+        if value < 0:
+            raise ValueError("price must be >= 0")
+        self._price = value
+
+    @property
+    def cost(self):
+        return self.shares * self.price
+
+    def sell(self, nshares):
+        self.shares -= nshares
 
 
 class DStock(Stock):
