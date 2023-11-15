2d0243bb37138d2d2cb633bcf9c9895f6f1d4aec
minocrafft <minocrafft@gmail.com>
2023-11-15 17:13:31 +0900

    Complete ~exercise9.4

--- structly/tableformat/__init__.py
+++ structly/tableformat/__init__.py
@@ -1,30 +1,4 @@
-from .formatter import TableFormatter, print_table
-from .formats.text import TextTableFormatter
-from .formats.csv import CSVTableFormatter
-from .formats.html import HTMLTableFormatter
+from .formatter import create_formatter, print_table
 
 
 __all__ = ["print_table", "create_formatter"]
-
-
-def create_formatter(types: str, column_formats=None, upper_headers=False):
-    if types.lower() == "text":
-        formatter = TextTableFormatter
-    elif types.lower() == "csv":
-        formatter = CSVTableFormatter
-    elif types.lower() == "html":
-        formatter = HTMLTableFormatter
-    else:
-        raise RuntimeError(f"Unknown format {types}")
-
-    if column_formats:
-
-        class formatter(ColumnFormatMixin, formatter):
-            formats = column_formats
-
-    if upper_headers:
-
-        class formatter(UpperHeadersMixin, formatter):
-            pass
-
-    return formatter()
--- structly/tableformat/formatter.py
+++ structly/tableformat/formatter.py
@@ -2,6 +2,13 @@ from abc import ABC, abstractmethod
 
 
 class TableFormatter(ABC):
+    _formats = {}
+
+    @classmethod
+    def __init_subclass__(cls):
+        name = cls.__module__.split(".")[-1]
+        TableFormatter._formats[name] = cls
+
     @abstractmethod
     def headings(self, headers):
         raise NotImplementedError
@@ -32,3 +39,24 @@ def print_table(records, fields, formatter):
     for record in records:
         rowdata = [getattr(record, fieldname) for fieldname in fields]
         formatter.row(rowdata)
+
+
+def create_formatter(name: str, column_formats=None, upper_headers=False):
+    if name not in TableFormatter._formats:
+        __import__(f"{__package__}.formats.{name}")
+
+    formatter_cls = TableFormatter._formats.get(name)
+    if not formatter_cls:
+        raise RuntimeError(f"Unknown format {name}")
+
+    if column_formats:
+
+        class formatter_cls(ColumnFormatMixin, formatter_cls):
+            formats = column_formats
+
+    if upper_headers:
+
+        class formatter_cls(UpperHeadersMixin, formatter_cls):
+            pass
+
+    return formatter_cls()
